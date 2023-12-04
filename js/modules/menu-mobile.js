33ed9d3fd8717a11bec68f3f471c6982857904bd
V.Rodrigues <vitorrodrigues06@outlook.com>
2023-12-04 09:49:10 -0300

    add animation showup with js  when close menu-mobile

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -3,6 +3,8 @@ import outsideEvent from "./outsideEvent.js";
 export default function initMenuMobile() {
   const menuButton = document.querySelector("#btn-mobile");
   const menuList = document.querySelector(".header-menu");
+  const itemMenu = document.querySelectorAll(".header-menu li");
+
   const events = ["click", "touchstart"];
 
   function openMenu(event) {
@@ -12,6 +14,16 @@ export default function initMenuMobile() {
     outsideEvent(menuList, events, () => {
       menuList.classList.remove("ativo");
       menuButton.classList.remove("ativo");
+      menuList.classList.add("closing");
+      menuButton.classList.add("closing");
+      menuList.addEventListener(
+        "animationend",
+        () => {
+          menuList.classList.remove("closing");
+          menuButton.classList.remove("closing");
+        },
+        { onde: true }
+      );
     });
   }
 
