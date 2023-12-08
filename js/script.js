89bfe66b4a661d988a69edf0bb72b5a634ee1478
V.Rodrigues <vitorrodrigues06@outlook.com>
2023-12-08 11:57:14 -0300

    add animações na introdução

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 import initMenuMobile from "./modules/menu-mobile.js";
 import initModal from "./modules/modal.js";
 import toTop from "./modules/toTopPage.js";
+import initAnime from "./modules/simple-anime.js";
 
 initMenuMobile();
 initModal();
 toTop();
+initAnime();
