cf09a61df95e2d2d4af5c2bd47d95d70a91a7d74
V.Rodrigues <vitorrodrigues06@outlook.com>
2024-05-06 10:09:28 -0300

    fix animação do btnTop

--- js/modules/toTopPage.js
+++ js/modules/toTopPage.js
@@ -1,5 +1,6 @@
 export default function toTop() {
   const goTop = document.querySelector(".top");
+  const header = document.querySelector(".introducao-bg");
 
   function pageTop(event) {
     event.preventDefault();
@@ -9,6 +10,26 @@ export default function toTop() {
     };
     window.scrollTo(options);
   }
+
+  function showBtnTop(entries, observer) {
+    const [entry] = entries;
+
+    if (entry.isIntersecting === true) {
+      goTop.classList.add("hideBtn");
+    }
+
+    if (entry.isIntersecting === false) {
+      goTop.classList.remove("hideBtn");
+      goTop.style.display = "flex";
+    }
+  }
+  const observer = new IntersectionObserver(showBtnTop, {
+    root: null,
+    threshold: 0,
+    rootMargin: "-400px",
+  });
+
+  observer.observe(header);
 
   goTop.addEventListener("click", pageTop);
 }
